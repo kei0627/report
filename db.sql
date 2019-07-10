b35f8e15a82e25c1b32e731b279b7e9c225fb280
kei0627 <49933981+kei0627@users.noreply.github.com>
2019-07-10 16:20:30 +0900

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
 create table player ( id int auto_increment not null primary key, name varchar(100), country varchar(100) );
 create table team ( id int auto_increment not null primary key, name varchar(100) );
-create table final ( id int auto_increment not null primary key, year int, team_id int, score of win int, team_id int, score of lose int, player_id int, King's score int, Man Of the Match int );
+create table final ( id int auto_increment not null primary key, year int, team_id int, win'sscore int, team_id int, lose'sscore int, player_id int, King'sscore int, Man Of the Match int );
 load data local infile 'player.csv' into table player fields terminated by ',' enclosed by '"';
 load data local infile 'team.csv' into table team fields terminated by ',' enclosed by '"';
 load data local infile 'final.csv' into table final fields terminated by ',' enclosed by '"';
