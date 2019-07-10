cd6c2cf1aec6613fffdff8a602f7e321360c60af
kei0627 <49933981+kei0627@users.noreply.github.com>
2019-07-10 16:12:52 +0900

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,4 +3,4 @@ create table team ( id int auto_increment not null primary key, name varchar(100
 create table final ( id int auto_increment not null primary key, year int, team_id int, score of win int, team_id int, score of lose int, player_id int, King's score int, Man Of the Match int );
 load data local infile 'player.csv' into table player fields terminated by ',' enclosed by '"';
 load data local infile 'team.csv' into table team fields terminated by ',' enclosed by '"';
-load data local infile 'final.csv' into table batting fields terminated by ',' enclosed by '"';
+load data local infile 'final.csv' into table final fields terminated by ',' enclosed by '"';
