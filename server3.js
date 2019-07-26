e35128bdbb43bf462b23064ec37a3bfac784c5f5
kei0627 <49933981+kei0627@users.noreply.github.com>
2019-07-26 16:49:20 +0900

    Create server3.js

---
+++ server3.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const server = express();
+const ejs = require('ejs');
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'node',
+    password: 'websystem',
+    database: 'web'
+});
+
+server.get('/', function( req, res ) {
+    let sorting = req.query.sorting || 'King';
+    let number = req.query.number || 10;
+    let query = 'select final.year,team.team_name,final.win,final.lose,team2.team2_name,player.player_name,' + sorting + ' from final inner join team on final.team_id = team.id inner join team2 on final.team2_id = team2.id inner join player on final.player_id = player.id order by ' + sorting + ' desc limit ' + number + ';';
+    console.log( query );
+    connection.query( query, (error, rows, fields) => {
+        if( error ) {
+            console.log('Query Error');
+        }
+        res.render( 'sql2.ejs', { content: rows });
+    });
+});
+
+server.listen( 80, function() {
+    console.log( 'listening on port 80' );
+});
