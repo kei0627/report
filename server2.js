6208b0ae41dbb79424784872f965feb19ce41ac3
kei0627 <49933981+kei0627@users.noreply.github.com>
2019-08-28 08:55:15 +0900

    Update server2.js

--- server2.js
+++ server2.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 server.get('/', function( req, res ) {
     let sorting = req.query.sorting || 'year';
     let number = req.query.number || 10;
-    let query = 'select final.year,team.team_name,player.player_name,final.win,final.lose,' + sorting + ' from final inner join team on final.team_id = team.id inner join player on final.player_id = player.id order by ' + sorting + ' desc limit ' + number + ';';
+    let query = 'select final.year,team.team_name,player.player_name,final.King,final.win,final.lose,' + sorting + ' from final inner join team on final.team_id = team.id inner join player on final.player_id = player.id order by ' + sorting + ' desc limit ' + number + ';';
     console.log( query );
     connection.query( query, (error, rows, fields) => {
         if( error ) {
